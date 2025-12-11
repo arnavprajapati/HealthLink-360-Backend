5123bda56aef28bce0f34297a927b3f3984d7fef
IamGaurav001 <hellogauravdubey@gmail.com>
2025-12-11 22:51:54 +0530

    -

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -11,6 +11,7 @@ export const protect = async (req, res, next) => {
                 message: 'Not authorized, no token'
             });
         }
+        //commit
 
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
